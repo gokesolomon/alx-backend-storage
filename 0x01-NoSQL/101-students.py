16a953d235c7170cd02d416678ab47748dc489c3
gokesolomon <gokehsolomon@gmail.com>
2024-03-24 10:45:22 -0700

    nosql

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+The students
+"""
+
+
+def top_students(mongo_collection):
+    """ the students by score """
+    return mongo_collection.aggregate([
+        {
+            "$project":
+                {
+                    "name": "$name",
+                    "averageScore": {"$avg": "$topics.score"}
+                }
+        },
+        {
+            "$sort":
+                {
+                    "averageScore": -1
+                }
+        }
+    ])
